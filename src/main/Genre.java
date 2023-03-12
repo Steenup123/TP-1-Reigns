9810a6a207b04d0fdcd7c74b948b3947a2f690ca
Steenup123 <2019602645@lacatholille.fr>
2023-03-12 22:34:18 +0100

    organiser les deux fichiers

--- src/main/Genre.java
+++ src/main/Genre.java
@@ -1,28 +1,10 @@
-package main;
+package com.fges.tp_solid.reigns.Personnage;
 
-/**
- * Représente les différents genres possibles.
- *
- * @author Julie Jacques / Lucien Mousin
- * @version 1.0
- */
 public enum Genre {
-    /**
-     * Genre masculin
-     */
-    ROI,
-    /**
-     * Genre féminin
-     */
-    REINE;
+    ROI, REINE;
 
-    /**
-     * Retourne une chaîne de caractères indiquant "Long règne au roi" ou "Long règne à la reine"
-     *
-     * @return une chaîne de caractères indiquant "Long règne au roi" ou "Long règne à la reine"
-     */
-    public String longRegne(){
-        if(this==ROI)
+    public String longRegne() {
+        if (this == ROI)
             return "Long règne au roi ";
         return "Long règne à la reine";
     }
